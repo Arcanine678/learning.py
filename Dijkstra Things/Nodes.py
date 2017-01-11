208a4de88e7068d182f0b0743ff447389568dada
Phire <xxyyxxnnxx@gmail.com>
2017-01-11 15:54:05 +1100

    Update Nodes.py

--- Dijkstra Things/Nodes.py
+++ Dijkstra Things/Nodes.py
@@ -11,6 +11,7 @@ class Node:
         self.weight = 100
         self.start = False
         self.end = False
+        self.closed = False
         self.bridges = []
         self.bridgesWeight = []
         print ("Node %d created"  % (self.name,))
@@ -22,16 +23,14 @@ class Node:
         
     def listBridges(self):
         print ("The bridges for %d are:" % ( self.name, ))
-        if (len(self.bridges) == 0):
-            print("there are no bridges for " + self.name)
-        else:
-            for index in range(len(self.bridges)):
-                print (self.bridges[index])
-                print (self.bridgesWeight[index])
+        bridges = [[] for i in range(len(self.bridges))]
+        for index in range(len(self.bridges)):
+            bridges[index].append(self.bridges[index])
+            bridges[index].append(self.bridgesWeight[index])
+        return bridges
 
-
-def Wait():
-    time.sleep(0.5)
+def wait():
+    time.sleep(5)
 
 def start():
 
@@ -53,8 +52,8 @@ def start():
     node[0].addBridge(2, 1)
     node[0].addBridge(3, 2)
     print()
-    node[1].addBridge(2, 3)
-    node[1].addBridge(5, 2)
+    node[1].addBridge(2, 2)
+    node[1].addBridge(5, 3)
     print()
     node[2].addBridge(0, 1)
     node[2].addBridge(1, 2)
@@ -76,21 +75,51 @@ def start():
     node[6].addBridge(5, 1)
     print()
     print()
-    node[0].listBridges()
-    node[1].listBridges()
-    node[2].listBridges()
-    node[3].listBridges()
-    node[4].listBridges()
-    node[5].listBridges()
-    node[6].listBridges()
+    print(node[0].listBridges())
+    print(node[1].listBridges())
+    print(node[2].listBridges())
+    print(node[3].listBridges())
+    print(node[4].listBridges())
+    print(node[5].listBridges())
+    print(node[6].listBridges())
     
     node[0].start = True
     node[0].weight = 0
     node[5].end = True
 
-    while (node[5].weight == 100):
-        print("No end found yet")
+    working = True
+    lastNode = 10
+    while (working):
+        currentNode = 4
+        for i in range(7):
+            if (node[i].weight < node[currentNode].weight) and (node[i].closed == False):
+                currentNode = i;
+        currentBridges = node[currentNode].listBridges();
         
-        Wait()
+        if currentNode == lastNode:
+            print("The end has been found")
+            break
+        else:
+            print("No end found yet")
+        
+
+        for index in range(len(currentBridges)):
+            if (node[currentBridges[index][0]].weight > node[currentNode].weight + currentBridges[index][1]):
+                node[currentBridges[index][0]].weight = node[currentNode].weight + currentBridges[index][1]
+            print(node[currentBridges[index][0]].weight)
+                
+        
+        node[currentNode].closed = True
+        
+        lastNode = currentNode;
+        wait()
+
+    print(node[0].weight)
+    print(node[1].weight)
+    print(node[2].weight)
+    print(node[3].weight)
+    print(node[4].weight)
+    print(node[5].weight)
+    print(node[6].weight)
 
 start()
